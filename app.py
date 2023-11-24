7bcb43c8afd7598572b3dc0d512c94f095162a17
pap5183 <pranavarun05@gmail.com>
2023-11-24 14:04:17 +0400

    made grades.html and integrated

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from flask import Flask, render_template, session,request, redirect, url_for, flash, jsonify
 from dbconfig import *
 from controller import *
-import bcrypt  # For password hashing
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Set a secret key for flashing messages
-
-    
-# Database configuration
 
 
 @app.route('/')
 def login():
     return render_template('login.html')
+
 
 
 @app.route('/login', methods=['POST'])
@@ -60,6 +57,8 @@ def teacher():
         # Redirect to the login page if the user is not logged in
         return redirect(url_for('login'))
 
+
+
 @app.route('/parent', methods=['GET'])
 def parent():
     # Check if the user is logged in
@@ -72,6 +71,8 @@ def parent():
         # Redirect to the login page if the user is not logged in
         return redirect(url_for('login'))
 
+
+
 @app.route('/counsellor', methods=['GET'])
 def counsellor():
     # Check if the user is logged in
@@ -83,6 +84,8 @@ def counsellor():
     else:
         # Redirect to the login page if the user is not logged in
         return redirect(url_for('login'))
+
+
 
 @app.route('/students', methods=['GET'])
 def students():
@@ -120,6 +123,7 @@ def students():
     return render_template('students.html', students_list=students_list, role_id=role_id)
     
 
+
 @app.route('/add_student', methods=['POST'])
 def add_student():
     data = request.json
@@ -128,6 +132,41 @@ def add_student():
     create_student(student_name, 1, 1, 1)
 
     return jsonify({'status': 'success'})
+
+
+@app.route('/grades')
+def grades():
+    if 'username' in session and 'role_id' in session:
+        user_id = session['user_id']
+        logged_in_user = session['username']
+        role_id = session['role_id']
+        
+    student_name = request.args.get('student', '')
+
+    cursor.execute("SELECT * FROM students WHERE student_name = %s", (student_name,))
+    student_details = cursor.fetchone()
+
+    cursor.execute("SELECT * FROM grades WHERE student_id = %s", (student_details['student_id'],))
+    grades_list = [(grades['subject'], grades['score'], grades['date']) for grades in cursor.fetchall()]
+
+    return render_template('grades.html', grades_list=grades_list, role_id=role_id)
+
+
+@app.route('/add_grade', methods=['POST'])
+def add_grade():
+    data = request.json
+    student_name = data['name']
+    subject = data['subject']
+    grade = data['grade']
+    date = data['date']
+
+    cursor.execute("SELECT student_id FROM students WHERE student_name = %s", (student_name,))
+    student_id = cursor.fetchone()['student_id']
+    
+    create_grade(student_id, 1, subject, grade, date)
+
+    return jsonify({'status': 'success'})
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -141,6 +180,7 @@ def register():
         create_user(username, password, email, role_id)
         flash('Registration successful. You can now login.', 'success')
         return redirect(url_for('login'))
+
 
 
 if __name__ == '__main__':
